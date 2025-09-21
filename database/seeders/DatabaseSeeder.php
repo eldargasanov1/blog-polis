7ce26bdb2255d69c857ee45fe299fa4c539675d5
Eldar <eldarforlife1@gmail.com>
2025-09-21 19:39:42 +0300

    added dev and prod docker environments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $users = User::factory()->count(50)->create();
-        $articles = Article::factory()->count(5)->create();
+        $articles = Article::factory()->count(500)->create();
 
         foreach ($users as $user) {
             $articles->random(3)->each(function ($article) use ($user) {
